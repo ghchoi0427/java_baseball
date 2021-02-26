89b885d0be2287719f9ef04d469c7a9e61ca330f
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-26 17:01:22 +0900

    feat: 메인 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,34 @@
 package baseball;
 
+import utils.Data;
+import utils.GameManager;
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        GameManager gm = new GameManager();
+        Data data = gm.data;
+        do {
+            gm.generateTarget();
+
+            do {
+                System.out.print("input number : ");
+                int input = scanner.nextInt();
+                gm.inputShot(input);
+                gm.clearScore();
+                gm.judge();
+                gm.printScore();
+            } while (data.getStrike() != 3);
+                gm.gameSet();
+                int cmd = scanner.nextInt();
+                gm.inputCmd(cmd);
+
+        } while (data.getCmd() != 2);
     }
 }
