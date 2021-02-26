d0a0031c721e703e0be386160675f489305b5018
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-26 21:04:11 +0900

    refactor: 파라미터 넘기기 간소화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import utils.Data;
 import utils.GameManager;
-
 import java.util.Scanner;
 
 public class Application {
@@ -11,21 +10,18 @@ public class Application {
         // TODO 구현 진행
         GameManager gm = new GameManager();
         Data data = gm.data;
+
         do {
             gm.generateTarget();
-
             do {
                 System.out.print("input number : ");
-                int input = scanner.nextInt();
-                gm.inputShot(input);
+                gm.inputShot(scanner.nextInt());
                 gm.clearScore();
                 gm.judge();
                 gm.printScore();
             } while (data.getStrike() != 3);
             gm.gameSet();
-            int cmd = scanner.nextInt();
-            gm.inputCmd(cmd);
-
+            gm.inputCmd(scanner.nextInt());
         } while (data.getCmd() != 2);
     }
 }
