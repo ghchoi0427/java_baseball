f938fa31e0f7c3ddc8a00964ae2e6fba80c50038
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-26 12:20:42 +0900

    feat: generateTarget 메서드 생성

--- src/main/java/utils/GameManager.java
+++ src/main/java/utils/GameManager.java
@@ -1,5 +1,26 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameManager {
+
+    public List<Integer> generateTarget() {
+        List<Integer> target = new ArrayList<>();
+        int temp;
+
+        target.add(RandomUtils.nextInt(1, 9));
+
+        for (int i = 0; i < 2; i++) {
+            do {
+                temp = RandomUtils.nextInt(1, 9);
+            } while (target.contains(temp));
+            target.add(temp);
+        }
+
+        return target;
+    }
+
 
 }
