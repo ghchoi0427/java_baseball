b2c3f768f8f93e195da4d7bb8c46681311392ff4
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-26 11:59:18 +0900

    feat: Data 클래스 추가

---
+++ src/main/java/utils/Data.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.util.List;
+
+public class Data {
+
+    List<Integer> target;
+    List<Integer> shot;
+    int cmd;
+
+    public List<Integer> getTarget() {
+        return target;
+    }
+
+    public void setTarget(List<Integer> target) {
+        this.target = target;
+    }
+
+    public List<Integer> getShot() {
+        return shot;
+    }
+
+    public void setShot(List<Integer> shot) {
+        this.shot = shot;
+    }
+
+    public int getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(int cmd) {
+        this.cmd = cmd;
+    }
+}
