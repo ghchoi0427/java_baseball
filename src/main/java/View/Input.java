b8384368a5804a044c6762557a8699549af3e1fd
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-02 23:30:53 +0900

    refactor: inputShot 메서드 Input 클래스로 이동, 생성자로 GameManager 주입

--- src/main/java/View/Input.java
+++ src/main/java/View/Input.java
@@ -1,4 +1,29 @@
 package View;
 
+import utils.GameManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Input {
+
+    GameManager gm;
+
+    public Input(GameManager gm) {
+        this.gm = gm;
+    }
+
+    public void inputShot(int input) {
+        List<Integer> shot = new ArrayList<>();
+
+        if ((int) (Math.log10(input) + 1) != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        shot.add(input / 100);
+        shot.add((input / 10) % 10);
+        shot.add(input % 10);
+
+        gm.data.setShot(shot);
+    }
 }
