a054d5da5019932110c112ea607975a1eb28ce6d
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-03 00:11:28 +0900

    refactor: setSCore 분리

--- src/main/java/utils/GameManager.java
+++ src/main/java/utils/GameManager.java
@@ -19,31 +19,14 @@ public class GameManager {
         data.setTarget(target);
     }
 
-    public void setScore() {
-        List<Integer> shot = data.getShot();
-        List<Integer> target = data.getTarget();
-        int ball = 0;
-        int strike = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                ball += countBall(target.get(i), i, shot.get(j), j);
-                strike += countStrike(target.get(i), i, shot.get(j), j);
-            }
-        }
-
-        data.setBall(ball);
-        data.setStrike(strike);
-    }
-
-    int countBall(int target, int targetIdx, int shot, int shotIdx) {
+    public int countBall(int target, int targetIdx, int shot, int shotIdx) {
         if (targetIdx != shotIdx && target == shot) {
             return 1;
         }
         return 0;
     }
 
-    int countStrike(int target, int targetIdx, int shot, int shotIdx) {
+    public int countStrike(int target, int targetIdx, int shot, int shotIdx) {
         if (targetIdx == shotIdx && target == shot) {
             return 1;
         }
