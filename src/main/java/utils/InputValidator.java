2a336ceab6583efc37df38f240a68b3c465c3c32
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-03 00:00:30 +0900

    refactor: InputValidator클래스로 validation 기능 분리

---
+++ src/main/java/utils/InputValidator.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class InputValidator {
+
+    public void validateShotInput() throws IllegalArgumentException{
+
+    }
+
+    public void validateCmdInput()throws IllegalArgumentException{
+
+    }
+}
