4fc264d442e21c39190ae6e539b51b09cfdb7958
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-03 00:05:50 +0900

    feat: validateCmdInput 메서드 구현

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -5,15 +5,17 @@ public class InputValidator {
     public void validateShotInput(int input) throws IllegalArgumentException {
 
         if ((int) (Math.log10(input) + 1) != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("input length should be 3");
         }
 
         if (input < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("input can't be below zero");
         }
     }
 
-    public void validateCmdInput() throws IllegalArgumentException {
-
+    public void validateCmdInput(int cmd) throws IllegalArgumentException {
+        if (cmd != 1 && cmd != 2) {
+            throw new IllegalArgumentException("command input should be 1 or 2");
+        }
     }
 }
