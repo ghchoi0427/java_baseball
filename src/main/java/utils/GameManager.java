1920508fce319d003d618c6b5c8685ba6d08f8bf
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-02 23:38:52 +0900

    refactor: Output 클래스로 출력 메서드 이동

--- src/main/java/utils/GameManager.java
+++ src/main/java/utils/GameManager.java
@@ -4,14 +4,9 @@ import java.util.*;
 
 public class GameManager {
 
-    private final String MESSAGE_INPUT = "input number : ";
-    private String PRINT_SCORE;
-    private final String MESSAGE_CONTINUE = "You got all 3 numbers correct! game set\nif you want to continue enter 1, to terminate enter 2";
+
     public Data data = new Data();
 
-    public void printInput() {
-        System.out.print(MESSAGE_INPUT);
-    }
 
     public void generateTarget() {
         List<Integer> target = new ArrayList<>();
@@ -27,19 +22,6 @@ public class GameManager {
         data.setTarget(target);
     }
 
-    public void inputShot(int input) {
-        List<Integer> shot = new ArrayList<>();
-
-        if ((int) (Math.log10(input) + 1) != 3) {
-            throw new IllegalArgumentException();
-        }
-
-        shot.add(input / 100);
-        shot.add((input / 10) % 10);
-        shot.add(input % 10);
-
-        data.setShot(shot);
-    }
 
     public void setScore() {
         List<Integer> shot = data.getShot();
@@ -77,32 +59,5 @@ public class GameManager {
         data.setStrike(0);
     }
 
-    public void printScore() {
-        PRINT_SCORE = "";
 
-        if (data.getBall() != 0) {
-            PRINT_SCORE += data.getBall() + "ball ";
-        }
-
-        if (data.getStrike() != 0) {
-            PRINT_SCORE += data.getStrike() + "strike";
-        }
-
-        if (data.getStrike() == 0 && data.getBall() == 0) {
-            PRINT_SCORE += "nothing";
-        }
-
-        System.out.println(PRINT_SCORE);
-    }
-
-    public void printGameSet() {
-        System.out.println(MESSAGE_CONTINUE);
-    }
-
-    public void inputCmd(int cmd) {
-        if (cmd != 1 && cmd != 2) {
-            throw new IllegalArgumentException();
-        }
-        data.setCmd(cmd);
-    }
 }
