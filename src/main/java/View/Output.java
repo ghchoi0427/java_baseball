809cf6e7155422381799b0b471f2ca88460041d4
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-02 23:38:42 +0900

    refactor: Output 클래스로 출력 메서드 이동

--- src/main/java/View/Output.java
+++ src/main/java/View/Output.java
@@ -1,4 +1,43 @@
 package View;
 
+import utils.GameManager;
+
 public class Output {
+
+    private final String MESSAGE_INPUT = "input number : ";
+    private String PRINT_SCORE;
+    private final String MESSAGE_CONTINUE = "You got all 3 numbers correct! game set\nif you want to continue enter 1, to terminate enter 2";
+
+    GameManager gm;
+
+    public Output(GameManager gm) {
+        this.gm = gm;
+    }
+
+    public void printInput() {
+        System.out.print(MESSAGE_INPUT);
+    }
+
+    public void printScore() {
+        PRINT_SCORE = "";
+
+        if (gm.data.getBall() != 0) {
+            PRINT_SCORE += gm.data.getBall() + "ball ";
+        }
+
+        if (gm.data.getStrike() != 0) {
+            PRINT_SCORE += gm.data.getStrike() + "strike";
+        }
+
+        if (gm.data.getStrike() == 0 && gm.data.getBall() == 0) {
+            PRINT_SCORE += "nothing";
+        }
+
+        System.out.println(PRINT_SCORE);
+    }
+
+    public void printGameSet() {
+        System.out.println(MESSAGE_CONTINUE);
+    }
+
 }
