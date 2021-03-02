80e5efe60648150612f70771e6fde657589f9d2a
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-03 00:02:46 +0900

    feat: validateShotInput 메서드 구현

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -2,11 +2,18 @@ package utils;
 
 public class InputValidator {
 
-    public void validateShotInput() throws IllegalArgumentException{
+    public void validateShotInput(int input) throws IllegalArgumentException {
 
+        if ((int) (Math.log10(input) + 1) != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        if (input < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void validateCmdInput()throws IllegalArgumentException{
+    public void validateCmdInput() throws IllegalArgumentException {
 
     }
 }
