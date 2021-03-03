f6b4f233f0a351394613153dea740a71b6349fa2
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-03 09:12:48 +0900

    refactor: startInning 메서드로 분리

--- src/main/java/utils/GameManager.java
+++ src/main/java/utils/GameManager.java
@@ -11,33 +11,26 @@ public class GameManager {
     Input input = new Input(this);
     Output output = new Output(this);
     ScoreManager sm = new ScoreManager(this);
+    TargetShotFactory targetShotFactory = new TargetShotFactory(this);
 
-    public void generateTarget() {
-        List<Integer> target = new ArrayList<>();
-        int temp;
-
-        for (int i = 0; i < 3; i++) {
-            do {
-                temp = RandomUtils.nextInt(1, 9);
-            } while (target.contains(temp));
-            target.add(temp);
-        }
-        data.setTarget(target);
-    }
 
     public void startGame(Scanner scanner){
         do {
-            generateTarget();
+            targetShotFactory.generateTarget();
             do {
-                output.printInput();
-                input.inputShot(scanner.nextInt());
-                sm.clearScore();
-                sm.setScore();
-                output.printScore();
+                startInning(scanner);
             } while (data.getStrike() != 3);
             output.printGameSet();
             input.inputCmd(scanner.nextInt());
         } while (data.getCmd() != 2);
+    }
+
+    public void startInning(Scanner scanner){
+        output.printInput();
+        input.inputShot(scanner.nextInt());
+        sm.clearScore();
+        sm.setScore();
+        output.printScore();
     }
 
 
