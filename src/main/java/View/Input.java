f1c1b10989c59a730b33f2bb3fdeb9505adec7c6
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-03 09:07:20 +0900

    feat: TargetShotFactory inputShot, inputCmd 메서드 생성

--- src/main/java/View/Input.java
+++ src/main/java/View/Input.java
@@ -2,28 +2,20 @@ package View;
 
 import utils.GameManager;
 import utils.InputValidator;
-
-import java.util.ArrayList;
-import java.util.List;
+import utils.TargetShotFactory;
 
 public class Input {
 
     GameManager gm;
     InputValidator inputValidator= new InputValidator();
-
     public Input(GameManager gm) {
         this.gm = gm;
     }
 
     public void inputShot(int input) {
-        List<Integer> shot = new ArrayList<>();
 
         inputValidator.validateShotInput(input);
-        shot.add(input / 100);
-        shot.add((input / 10) % 10);
-        shot.add(input % 10);
-
-        gm.data.setShot(shot);
+        new TargetShotFactory(gm).generateShot(input);
     }
 
     public void inputCmd(int cmd) {
--- src/main/java/utils/TargetShotFactory.java
+++ src/main/java/utils/TargetShotFactory.java
@@ -1,12 +1,34 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TargetShotFactory {
 
-    public void generateTarget(){
+    GameManager gm;
 
+    public TargetShotFactory(GameManager gm) {
+        this.gm = gm;
     }
 
-    public generateShot(int input){
+    public void generateTarget() {
+        List<Integer> target = new ArrayList<>();
+        int temp;
 
+        for (int i = 0; i < 3; i++) {
+            do {
+                temp = RandomUtils.nextInt(1, 9);
+            } while (target.contains(temp));
+            target.add(temp);
+        }
+        gm.data.setTarget(target);
+    }
+
+    public void generateShot(int input){
+        List<Integer> shot = new ArrayList<>();
+        shot.add(input / 100);
+        shot.add((input / 10) % 10);
+        shot.add(input % 10);
+        gm.data.setShot(shot);
     }
 }
