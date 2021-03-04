6f4db49c12b461d29dacd6a9a507715b402d8528
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 10:15:10 +0900

    refactor: Stream을 사용한 validation

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class InputValidator {
 
     public void validateShotInput(int input) throws IllegalArgumentException {
@@ -10,6 +14,25 @@ public class InputValidator {
 
         if (input < 0) {
             throw new IllegalArgumentException("input can't be below zero");
+        }
+    }
+
+    public void validateShotInput(List<Integer> input) throws IllegalArgumentException {
+        Stream<Integer> inputStream1 = input.stream();
+        Stream<Integer> inputStream2 = input.stream();
+        Stream<Integer> inputStream3 = input.stream();
+        Stream<Integer> inputStream4 = input.stream();
+
+        if (inputStream1.anyMatch(n -> n == 0)) {
+            throw new IllegalArgumentException("input shouldn't include zero");
+        }
+
+        if (inputStream2.mapToLong(e->1L).sum() != 3) {
+            throw new IllegalArgumentException("input length should be three");
+        }
+
+        if (inputStream3.count() != inputStream4.distinct().count()) {
+            throw new IllegalArgumentException("cannot include overlapped numbers");
         }
     }
 
