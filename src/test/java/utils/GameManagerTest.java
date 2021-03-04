0aa0f5981e0fb937254efdfbffcf01ce067bb13f
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 10:28:55 +0900

    refactor: 줏거처리

--- src/test/java/utils/GameManagerTest.java
+++ src/test/java/utils/GameManagerTest.java
@@ -9,13 +9,5 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameManagerTest {
     GameManager gameManager = new GameManager();
 
-    @Test
-    public void checkOverlap(){
-        List<Integer> list = gameManager.generateTarget();
-        assertTrue(list.get(0)!=list.get(1) && list.get(1)!=list.get(2) && list.get(0)!=list.get(2));
-        /*for(int i : list){
-            System.out.println("element: "+i);
-        }*/
-    }
 
 }
