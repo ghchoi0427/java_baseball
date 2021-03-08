9b195df4fa1c6d817057599ed3661c6324845205
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-08 22:34:18 +0900

    refactor: gameManager에 대한 의존 없애기, util성 메서드는 static으로 선언

--- src/main/java/View/Output.java
+++ src/main/java/View/Output.java
@@ -1,42 +1,36 @@
 package View;
 
-import utils.GameManager;
+import Model.ScoreDto;
 
 public class Output {
 
-    private final String MESSAGE_INPUT = "input number : ";
-    private String PRINT_SCORE;
-    private final String MESSAGE_CONTINUE = "You got all 3 numbers correct! game set\nif you want to continue enter 1, to terminate enter 2";
+    private static final String MESSAGE_INPUT = "input number : ";
+    private static String PRINT_SCORE;
+    private static final String MESSAGE_CONTINUE = "You got all 3 numbers correct! game set\nif you want to continue enter 1, to terminate enter 2";
 
-    GameManager gm;
-
-    public Output(GameManager gm) {
-        this.gm = gm;
-    }
-
-    public void printInput() {
+    public static void printInput() {
         System.out.print(MESSAGE_INPUT);
     }
 
-    public void printScore() {
+    public static void printScore(ScoreDto score) {
         PRINT_SCORE = "";
 
-        if (gm.data.getBall() != 0) {
-            PRINT_SCORE += gm.data.getBall() + "ball ";
+        if (score.getBall() != 0) {
+            PRINT_SCORE += score.getBall() + "ball ";
         }
 
-        if (gm.data.getStrike() != 0) {
-            PRINT_SCORE += gm.data.getStrike() + "strike";
+        if (score.getStrike() != 0) {
+            PRINT_SCORE += score.getStrike() + "strike";
         }
 
-        if (gm.data.getStrike() == 0 && gm.data.getBall() == 0) {
+        if (score.getStrike() == 0 && score.getBall() == 0) {
             PRINT_SCORE += "nothing";
         }
 
         System.out.println(PRINT_SCORE);
     }
 
-    public void printGameSet() {
+    public static void printGameSet() {
         System.out.println(MESSAGE_CONTINUE);
     }
 
