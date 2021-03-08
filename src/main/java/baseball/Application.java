0b5a5145391d64b6cc5ed05c3e2488441ba8b79e
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-08 22:38:10 +0900

    refactor: gameManager 변화에 맞게 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,10 @@
 package baseball;
 
 import utils.GameManager;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
         GameManager gm = new GameManager();
-        gm.startGame(scanner);
+        gm.startGame();
     }
 }
