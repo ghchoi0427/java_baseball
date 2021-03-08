d5d554581b547ff6199c2fd5f62ada57b6135467
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-08 22:33:10 +0900

    refactor: gameManager에 대한 의존 없애기, 메서드 수정

--- src/main/java/utils/ScoreManager.java
+++ src/main/java/utils/ScoreManager.java
@@ -1,48 +1,38 @@
 package utils;
 
+import Model.ScoreDto;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ScoreManager {
 
-    GameManager gm;
+    public static int countBall(List<Integer> shot, List<Integer> target) {
+        int sum = 0;
 
-    public ScoreManager(GameManager gm) {
-        this.gm = gm;
+
+        sum = (int) IntStream.range(0, shot.size())
+                .filter(index -> isContainsDifferentPosition(target, shot.get(index), index))
+                .count();
+
+        return sum;
     }
 
-    public void setScore() {
-        List<Integer> shot = gm.data.getShot();
-        List<Integer> target = gm.data.getTarget();
-        int ball = 0;
-        int strike = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                ball += countBall(target.get(i), i, shot.get(j), j);
-                strike += countStrike(target.get(i), i, shot.get(j), j);
-            }
-        }
-
-        gm.data.setBall(ball);
-        gm.data.setStrike(strike);
+    private static boolean isContainsDifferentPosition(List<Integer> target, int number, int index) {
+        return target.contains(number) && target.get(index) != number;
     }
 
-    public int countBall(int target, int targetIdx, int shot, int shotIdx) {
-        if (targetIdx != shotIdx && target == shot) {
-            return 1;
-        }
-        return 0;
+
+    public static int countStrike(List<Integer> shot, List<Integer> target) {
+        int sum = 0;
+
+        sum = (int) IntStream.range(0, shot.size())
+                .filter(index -> shot.get(index) == target.get(index))
+                .count();
+
+        return sum;
     }
 
-    public int countStrike(int target, int targetIdx, int shot, int shotIdx) {
-        if (targetIdx == shotIdx && target == shot) {
-            return 1;
-        }
-        return 0;
-    }
-
-    public void clearScore() {
-        gm.data.setBall(0);
-        gm.data.setStrike(0);
+    public static ScoreDto countScore(List<Integer> shot, List<Integer> target) {
+        return new ScoreDto(countBall(shot, target), countStrike(shot, target));
     }
 }
