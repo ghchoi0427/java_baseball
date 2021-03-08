472fe254948f84da77a931487c96d45f1f176b0c
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-08 22:35:55 +0900

    refactor: util성 메서드는 static으로 선언, 재할당 없는 필드는 final로 선언

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -1,35 +1,30 @@
 package utils;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public class InputValidator {
 
-    final String Msg_zeroInput = "input shouldn't include zero";
-    final String Msg_illegalLength = "input length should be three";
-    final String Msg_overlap = "all three numbers should be distinct";
-    final String Msg_illegalCommand = "command should be 1 or 2";
+    private static final String Msg_zeroInput = "input shouldn't include zero";
+    private static final String Msg_illegalLength = "input length should be three";
+    private static final String Msg_overlap = "all three numbers should be distinct";
+    private static final String Msg_illegalCommand = "command should be 1 or 2";
 
-    public void validateShotInput(List<Integer> input) throws IllegalArgumentException {
-        Stream<Integer> inputStream1 = input.stream();
-        Stream<Integer> inputStream2 = input.stream();
-        Stream<Integer> inputStream3 = input.stream();
-        Stream<Integer> inputStream4 = input.stream();
+    public static void validateShotInput(List<Integer> input) throws IllegalArgumentException {
 
-        if (inputStream1.anyMatch(n -> n == 0)) {
+        if (input.stream().anyMatch(n -> n == 0)) {
             throw new IllegalArgumentException(Msg_zeroInput);
         }
 
-        if (inputStream2.count() != 3) {
+        if (input.stream().count() != 3) {
             throw new IllegalArgumentException(Msg_illegalLength);
         }
 
-        if (inputStream3.count() != inputStream4.distinct().count()) {
+        if (input.stream().count() != input.stream().distinct().count()) {
             throw new IllegalArgumentException(Msg_overlap);
         }
     }
 
-    public void validateCmdInput(int cmd) throws IllegalArgumentException {
+    public static void validateCmdInput(int cmd) throws IllegalArgumentException {
         if (cmd != 1 && cmd != 2) {
             throw new IllegalArgumentException(Msg_illegalCommand);
         }
